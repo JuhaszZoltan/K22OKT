796b529fa8b6997433061fa367eefe80830371e4
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2022-10-25 15:55:45 +0200

    Add project files.

---
+++ BukkMaraton2019/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace BukkMaraton2019
+{
+    internal class Program
+    {
+        static List<Versenyzo> versenyzok = new();
+        const int restvevokSzama = 691;
+
+        static void Main(string[] args)
+        {
+            Feladat03();
+            Feladat04();
+            Feladat05();
+            Feladat06();
+        }
+
+        private static void Feladat06()
+        {
+            bool b = versenyzok.Any(x => x.Ido.TotalHours > 6);
+            Console.WriteLine("6. feladat: " +
+                $"{(b ? "Volt ilyen versenyző" : "Nem volt ilyen versenyző")}");
+        }
+
+        private static void Feladat05()
+        {
+            int c = versenyzok.Count(x => !x.Nem && x.Versenytav == "Rövid");
+            Console.WriteLine($"5. feladat: " +
+                $"Nővi versenyzők száma a rövittávú versenyen: {c} fő");
+        }
+
+        private static void Feladat04()
+        {
+            Console.WriteLine($"4. feladat: " +
+                $"Versenytávot nem teljesítők: " +
+                $"{100 - (versenyzok.Count / (double)restvevokSzama * 100)}%");
+        }
+
+        private static void Feladat03()
+        {
+            using StreamReader sr = new(path: @"..\..\..\src\bukkm2019.txt");
+            _ = sr.ReadLine();
+            while (!sr.EndOfStream)
+                versenyzok.Add(new Versenyzo(sr.ReadLine()));
+        }
+    }
+}
---
+++ BukkMaraton2019/Versenyzo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BukkMaraton2019
+{
+    internal class Versenyzo
+    {
+        public string Rajtszam { get; set; }
+        public string Versenytav => Rajtszam[0] switch
+        {
+            'M' => "Mini",
+            'R' => "Rövid",
+            'K' => "Közép",
+            'H' => "Hosszú",
+            'E' => "Pedelec",
+            _ => "Hibás rajtszám"
+        };
+        public bool Nem => Kategoria.Last() == 'f';
+        public string Kategoria { get; set; }
+        public string Nev { get; set; }
+        public string? Egyesulet { get; set; }
+        public TimeSpan Ido { get; set; }
+        public Versenyzo(string r)
+        {
+            string[] v = r.Split(';');
+            Rajtszam = v[0];
+            Kategoria = v[1];
+            Nev = v[2];
+            Egyesulet = v[3];
+            Ido = TimeSpan.Parse(v[4]);
+        }
+    }
+}
